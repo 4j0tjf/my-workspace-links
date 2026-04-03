d6c42374ab19108816dc713e825ad4b13ba063a7
4j0tjf <4j0tjf@gmail.com>
2026-04-03 23:15:44 +0900

    의존성 경고 해결 및 화면 노출 버그 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserPage from './UserPage';
 import AdminPage from './AdminPage';
 
@@ -6,14 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 기본 주소(/)로 접속하면 UserPage를 보여줌 */}
         <Route path="/" element={<UserPage />} />
-        
-        {/* /admin 주소로 접속하면 AdminPage를 보여줌 */}
         <Route path="/admin" element={<AdminPage />} />
-        
-        {/* 그 외의 이상한 주소로 접속하면 기본 주소(/)로 튕겨냄 */}
-        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
